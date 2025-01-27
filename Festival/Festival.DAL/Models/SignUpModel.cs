c48fff785d49f0633222b5ef539dd5a22847b525
MSChobanov21 <MSChobanov21@codingburgas.bg>
2025-01-27 15:49:18 +0200

    Add Artist, Event and Ticket razor

--- Festival/Festival.DAL/Models/SignUpModel.cs
+++ Festival/Festival.DAL/Models/SignUpModel.cs
@@ -1,16 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Festival.DAL.Models;
 
-public class SignUpModel
+namespace Festival.DAL.Models
 {
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+    public class Model
+    {
+        [Required]
+        [EmailAddress]
+        public static string Email { get; set; } 
 
-    [Required]
-    [StringLength(100, MinimumLength = 6)]
-    public string Password { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public static string Password { get; set; } 
 
-    [Compare("Password")]
-    public string ConfirmPassword { get; set; }
+        [Compare("Password")]
+        public static string ConfirmPassword { get; set; } 
+    }
 }
